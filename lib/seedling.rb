e02564f83b063f6b85fadddc3083afc6d85cbba6
TJ Vanderpoel <bougy.man@gmail.com>
2009-05-28 19:47:07 -0500

    doh, removed the wrong parens

--- lib/seedling.rb
+++ lib/seedling.rb
@@ -11,9 +11,9 @@ class Pathname
   end
 end
 
-$LOAD_PATH.unshift ( Pathname(__FILE__).dirname ).expand_path.to_s
+$LOAD_PATH.unshift((Pathname(__FILE__).dirname).expand_path.to_s)
 module Seedling
   autoload :VERSION, "seedling/version"
-  ROOT = ( Pathname($LOAD_PATH.first)/".." ).expand_path unless Seedling.const_defined?("ROOT")
+  ROOT = (Pathname($LOAD_PATH.first)/"..").expand_path unless Seedling.const_defined?("ROOT")
   LIBDIR = ROOT/:lib
 end
